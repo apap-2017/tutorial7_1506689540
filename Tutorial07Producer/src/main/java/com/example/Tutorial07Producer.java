2efbeb9f90f78e8b6c2f6152ca3117c267797d53
asdfelisnd <felis.sts@outlook.com>
2017-11-03 21:52:35 +0700

    2

--- Tutorial07Producer/src/main/java/com/example/Tutorial07Producer.java
+++ Tutorial07Producer/src/main/java/com/example/Tutorial07Producer.java
@@ -4,11 +4,11 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class ApapTutorial04Application
+public class Tutorial07Producer
 {
 
     public static void main (String[] args)
     {
-        SpringApplication.run (ApapTutorial04Application.class, args);
+        SpringApplication.run (Tutorial07Producer.class, args);
     }
 }
